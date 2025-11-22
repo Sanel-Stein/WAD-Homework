b7f5138754e50f0afd0a1e4c27cbab4bc5269a48
Kaffite <karlmarkushannust@gmail.com>
2025-11-22 16:51:23 +0200

    Vue branch init

--- src/js/dropDownMenu.js
+++
@@ -1,8 +0,0 @@
-//Drop down 
-let menuIcon = document.querySelector(".menu_icon")
-let dropDownMenu = document.querySelector(".dropDownMenu")
-
-console.log("menuIcoN", menuIcon);
-menuIcon.addEventListener('click', () => {
-    dropDownMenu.classList.toggle("visible");
-})
---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { createStore } from 'vuex'
+
+
+export default createStore({
+    strict: true,
+    state: {
+            // productList should be placed here
+            posts: []
+    },
+  getters: {
+      },
+  mutations: {
+
+    },
+  actions: {
+    }
+})
